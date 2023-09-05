28885212f099c4c2c1a20edefdc06e86f9cfc1ac
brosnanyuen <brosnany@uvic.ca>
2023-09-04 21:21:55 -0700

    save

--- src/Util/Convert.rs
+++ src/Util/Convert.rs
@@ -3,3 +3,47 @@ use arrayfire;
 
 
 
+
+
+pub fn COO_to_CSR(
+	WRowIdxCOO: &arrayfire::Array<i32>,
+    row_num: u64
+    ) -> arrayfire::Array<i32>
+	{
+
+    let WRowIdxCOO_num  = WRowIdxCOO.dims()[0];
+
+
+    let ones = arrayfire::constant::<i32>(1,arrayfire::Dim4::new(&[WRowIdxCOO_num,1,1,1]));
+    let mut temparr = arrayfire::constant::<i32>(0,arrayfire::Dim4::new(&[row_num,1,1,1]));
+
+    let mut idxrs = arrayfire::Indexer::default();
+    idxrs.set_index(WRowIdxCOO, 0, None);
+    arrayfire::assign_gen(&mut temparr, &idxrs, &ones);
+
+    let sel = arrayfire::locate(&temparr);
+
+
+    //let  (_,mut sumarr) = arrayfire::count_by_key(WRowIdxCOO, &ones, 0);
+    let  (_,mut sumarr) = arrayfire::sum_by_key(WRowIdxCOO, &ones, 0);
+
+    sumarr = sumarr.cast::<i32>();
+
+
+    let mut idxrs = arrayfire::Indexer::default();
+    idxrs.set_index(&sel, 0, None);
+    arrayfire::assign_gen(&mut temparr, &idxrs, &sumarr);
+
+
+
+    temparr = arrayfire::accum(&temparr, 0);
+
+
+    let constarr = arrayfire::constant::<i32>(0,arrayfire::Dim4::new(&[1,1,1,1]));
+    temparr = arrayfire::join(0, &constarr, &temparr);
+
+    temparr
+}
+
+
+
