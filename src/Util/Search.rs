398dc56957da3d3424a9998eb360c1456653a4ec
brosnanyuen <brosnany@uvic.ca>
2023-09-05 23:13:14 -0700

    save

--- src/Util/Search.rs
+++ src/Util/Search.rs
@@ -107,4 +107,26 @@ pub fn COO_batch_find<Z: arrayfire::RealNumber>(
 
 
 
+pub fn find_unique(
+    arr: &arrayfire::Array<i32>,
+    neuron_size: u64
+    ) -> arrayfire::Array<i32>
+    {
+
+    let table_dims = arrayfire::Dim4::new(&[neuron_size,1,1,1]);
+    let mut table = arrayfire::constant::<bool>(false,table_dims);
+
+    let inarr = arrayfire::constant::<bool>(true, arr.dims());
+    //let idxarr = arr.cast::<u32>();
+
+    let mut idxrs = arrayfire::Indexer::default();
+    idxrs.set_index(arr, 0, None);
+    arrayfire::assign_gen(&mut table, &idxrs, &inarr);
+
+    arrayfire::locate(&table).cast::<i32>()
+}
+
+
+
+
 
