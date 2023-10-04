7a08dc7f8797270edb1d95ec3d18ced894e0029d
brosnanyuen <brosnany@uvic.ca>
2023-10-04 14:06:29 -0700

    save

--- src/Util/Remove.rs
+++ src/Util/Remove.rs
@@ -20,4 +20,30 @@ pub fn select_values<Z: arrayfire::FloatingPoint>(
 
 
 
+pub fn clear_input<Z: arrayfire::FloatingPoint>(
+    WValues: &mut arrayfire::Array<Z>,
+    WRowIdxCOO: &mut arrayfire::Array<i32>,
+    WColIdx: &mut arrayfire::Array<i32>,
+    input_rows: u64
+)
+{
+
+    let single = input_rows as i32;
+
+    //let cmp2 = (single <= WRowIdxCOO );
+    let cmp1 = arrayfire::le(&single ,WRowIdxCOO, false);
+
+    let sel = arrayfire::locate(&cmp1);
+
+    select_values::<Z>(
+        WValues,
+        WRowIdxCOO,
+        WColIdx,
+        &sel
+    );
+
+}
+
+
+
 
