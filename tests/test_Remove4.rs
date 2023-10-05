81829ce71ce91ac03bec3b22400e2862735bde37
brosnanyuen <brosnany@uvic.ca>
2023-10-04 18:33:59 -0700

    save

---
+++ tests/test_Remove4.rs
@@ -0,0 +1,21 @@
+
+use arrayfire;
+use RayBNN_Sparse;
+
+//Select CUDA and GPU Device 0
+const BACK_END: arrayfire::Backend = arrayfire::Backend::CUDA;
+const DEVICE: i32 = 0;
+
+
+
+
+#[test]
+fn test_Remove4() {
+
+	arrayfire::set_backend(BACK_END);
+	arrayfire::set_device(DEVICE);
+
+
+
+
+}
