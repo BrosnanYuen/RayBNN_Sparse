ea5b88013d28ff8c879074f9bdd15ea593bcab3f
brosnanyuen <brosnany@uvic.ca>
2023-10-30 16:30:45 -0700

    save

--- tests/test_Remove5.rs
+++ tests/test_Remove5.rs
@@ -206,4 +206,89 @@ fn test_Remove5() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    let WValues_cpu:Vec<f64> = vec![9.0,-2.8,5.1,-0.3,4.8,4.1, 1.7, -0.9, 0.3, -2.0, 5.0, -1.0, 0.42, -0.2, 0.1, 3.1, 10.0];
+	let mut WValues = arrayfire::Array::new(&WValues_cpu, arrayfire::Dim4::new(&[17, 1, 1, 1]));
+
+
+    let WRowIdxCOO_cpu:Vec<i32> = vec![0,0,1,1,1,2,2,3,3,4,4,5,5,5,6,6,6];
+	let mut WRowIdxCOO = arrayfire::Array::new(&WRowIdxCOO_cpu, arrayfire::Dim4::new(&[17, 1, 1, 1]));
+
+
+    let WColIdx_cpu:Vec<i32> =    vec![2,3,4,5,6,0,1,0,1,0,1,2,3,4,2,3,4];
+	let mut WColIdx = arrayfire::Array::new(&WColIdx_cpu, arrayfire::Dim4::new(&[17, 1, 1, 1]));
+
+
+
+    let delete_cpu:Vec<i32> = vec![1,3,4];
+	let mut delete_idx = arrayfire::Array::new(&delete_cpu, arrayfire::Dim4::new(&[delete_cpu.len() as u64, 1, 1, 1]));
+
+    RayBNN_Sparse::Util::Remove::delete_neurons_at_idx(
+        &delete_idx, 
+        &mut WValues, 
+        &mut WRowIdxCOO, 
+        &mut WColIdx
+    );
+    
+
+
+        
+    let mut WValues_act:Vec<f64> = vec![9.0, -0.3, 4.8, 4.1, -0.9, -2.0, -1.0, 0.1];
+    let mut WValues_cpu = vec!(f64::default();WValues.elements());
+
+    WValues.host(&mut WValues_cpu);
+
+    WValues_cpu = WValues_cpu.par_iter().map(|x|  (x * 1000000.0).round() / 1000000.0 ).collect::<Vec<f64>>();
+
+    WValues_act = WValues_act.par_iter().map(|x|  (x * 1000000.0).round() / 1000000.0 ).collect::<Vec<f64>>();
+
+    assert_eq!(WValues_cpu, WValues_act);
+
+
+
+    
+
+    let mut WRowIdxCOO_act:Vec<i32> = vec![0,1,1,2,3,4,5,6];
+    let mut WRowIdxCOO_cpu = vec!(i32::default();WRowIdxCOO.elements());
+
+
+    WRowIdxCOO.host(&mut WRowIdxCOO_cpu);
+
+    assert_eq!(WRowIdxCOO_cpu, WRowIdxCOO_act);
+
+    
+
+
+
+
+
+
+
+    let mut WColIdx_act:Vec<i32> = vec![2,5,6,0,0,0,2,2];
+    let mut WColIdx_cpu = vec!(i32::default();WColIdx.elements());
+
+
+    WColIdx.host(&mut WColIdx_cpu);
+
+    assert_eq!(WColIdx_cpu, WColIdx_act);
+
+
+
 }
